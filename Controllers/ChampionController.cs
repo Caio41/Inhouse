f636a7343a0127854dd00bd527d9575b2ff65b15
Caio <caiaoviniciuscc@gmail.com>
2024-04-18 01:30:01 -0300

    first commit

---
+++ Controllers/ChampionController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Inhouse.Controllers
+{
+    public class ChampionController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
