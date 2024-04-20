e7b0aaab503a70168f0fc9ab02d31b1ad856460c
Caio <caiaoviniciuscc@gmail.com>
2024-04-20 17:51:40 -0300

    testando match methods

---
+++ Controllers/JogadorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Inhouse.Controllers
+{
+    public class JogadorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
