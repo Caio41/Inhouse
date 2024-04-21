1164af42dfded55122028c09344f07553c5a83fe
Caio <caiaoviniciuscc@gmail.com>
2024-04-21 17:47:54 -0300

    add tabela

--- Controllers/JogadorController.cs
+++ Controllers/JogadorController.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Inhouse.Data;
+using Inhouse.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Inhouse.Controllers
 {
     public class JogadorController : Controller
     {
+        private readonly AppDbContext _db;
+
+        public JogadorController(AppDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+
+            IEnumerable<Jogador> jogadoresList = _db.Jogadores.ToList();
+            return View(jogadoresList);
+        }
+
+        public IActionResult Ranking()
+        {
+
+            IEnumerable<Jogador> jogadoresList = _db.Jogadores.OrderByDescending(j => j.Wins).ToList();
+            return View(jogadoresList);
         }
     }
 }
